56d21ab14589024d152624c5452f2d794a3c8b05
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-12-19 17:06:21 +0530

    ...

---
+++ Modules/main.js
@@ -0,0 +1,6 @@
+const math = require("./math");
+
+console.log(math.add(10, 20));
+console.log(math.sub(10, 20));
+console.log(math.div(20, 10));
+console.log(math.multiply(20, 10));
---
+++ Modules/math.js
@@ -0,0 +1,28 @@
+// Modules :
+// -> Modules is concept where we can access one modules(File) to another file.
+// -> In Modules we can access another files method and attributes in many defferent files
+// -> Its provides us reusablity and reduce line of code.
+
+
+function add(value1, value2) {
+    return value1 + value2;
+}
+
+
+function sub(value1, value2) {
+    return value1 - value2;
+}
+
+
+function div(value1, value2) {
+    return value1 / value2;
+}
+
+
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+
+module.exports = {
+    add, sub, div, multiply,
+};
