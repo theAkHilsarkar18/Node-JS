b686e038d06864764faf6db94543dc5286b4cb1e
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-12-20 17:45:01 +0530

    ....

--- File Handling/main.js
+++ File Handling/main.js
@@ -1,6 +1,21 @@
 const fs = require("fs");
 
-fs.writeFileSync(
-    './File Handling/demo.txt',
-    'Demo.txt is created !'
-);
+// to create a file in write mode - its override all passed string wheneve we run
+// its sync function
+// fs.writeFileSync(
+//     './File Handling/demo.txt',
+//     'Demo.txt is created !'
+// );
+
+// its async function
+// fs.writeFile('./File Handling/demo.txt','Its created by async',(err)=>{
+//     console.log(err);
+// });
+
+// to read existing file from storage
+// its return data from file
+// utf-8 : we provide one type of format of file to decode files content easily, utf-8 for txt file
+const result = fs.readFileSync('./File Handling/demo.txt',"utf-8");
+console.log(result);
+
+
