72854695ffff9f12f595c73fca20ee89e69a45d9
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2025-01-09 18:33:48 +0530

    .....

--- Express JS/USE/index.js
+++ Express JS/USE/index.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send("This is home page !");
+});
+
+app.post('/login', (request, response) => {
+    response.json(request.query);
+});
+
+
+
+app.listen(8000, () => {
+    console.log("Server started on http://localhost:8000");
+});
