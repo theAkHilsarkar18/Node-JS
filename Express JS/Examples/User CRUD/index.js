5d6320603c01e1559eaf986effef4f9f3ac83937
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2025-01-13 16:57:07 +0530

    ...

--- Express JS/Examples/User CRUD/index.js
+++ Express JS/Examples/User CRUD/index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const users = require("./users.json");
 const app = express();
 
+
+// /users -> all users
 app.get('/users', (request, response) => {
     response.json(users);
 });
 
+// /users/id -> one user of specific id from all users
 app.get('/users/:id', (request, response) => {
     const id = request.params.id;
     response.json(users.find(user => user.id == id));
 });
 
+// to start server on 8000
 app.listen(8000, () => {
     console.log("Server is started on http://localhost:8000");
 });
