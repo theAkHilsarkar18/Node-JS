a39a30ca22a6e087cd9a302456e561c8cf2a6eb8
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2025-01-17 17:52:38 +0530

    ...

--- Express JS/Examples/User CRUD/index.js
+++ Express JS/Examples/User CRUD/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./users.json");
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
 
 // /users -> all users
 app.get('/users', (request, response) => {
@@ -14,7 +16,30 @@ app.get('/users/:id', (request, response) => {
     response.json(users.find(user => user.id == id));
 });
 
+// to add user
+app.post('/users', (request, response) => {
+    // const { first_name, last_name, email, gender, job_title } = request.body;
+
+    // if (!first_name || !last_name || !email || !gender || !job_title) {
+    //     response.status(400);
+    //     response.json({ error: 'All fields are required' });
+    // }
+
+
+
+    const body = request.body;
+    users.push(body);
+    console.log(body);
+    fs.writeFile("./users.json", JSON.stringify(users), (err, data) => {
+        return response.json({ 'status': 'User added !', 'err': err });
+    });
+
+
+});
+
 // to start server on 8000
 app.listen(8000, () => {
     console.log("Server is started on http://localhost:8000");
 });
+
+// krish,keyur,ravi,jils,jenish,divyesh :-
